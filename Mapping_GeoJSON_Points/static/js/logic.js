360852d5a04d4dda43abb38c42aa63315917358d
EMish <Liza.s.mishina@gmail.com>
2020-10-09 23:19:50 -0700

    13.5.4

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -7,10 +7,23 @@ console.log("working");
 //let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+//let map = L.map('mapid').setView([30, 30], 2);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/ElizMishina/Mapping_Earthquakes/main/majorAirports.json";
+
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    L.geoJson(data,{
+        pointToLayer: function(feature, latlng) {
+          console.log(feature);
+          return L.marker(latlng)
+          .bindPopup("<h3>" + "Ariport code: " + feature.properties.faa + "<h3>" + "<h3>" + "Ariport name: " +feature.properties.name + "</h3>")
+          }
+      }).addTo(map);
+
+
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -18,9 +31,36 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+
+  // Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+ 
+ /*   // Creating a GeoJSON layer with the retrieved data.
+  
+
+
+*/
 // Add GeoJSON data.
 /*let sanFranAirport =
 {"type":"FeatureCollection","features":[{
@@ -54,14 +94,3 @@ L.geoJson(sanFranAirport, {
   }).addTo(map);*/
 
   // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data,{
-      pointToLayer: function(feature, latlng) {
-        console.log(feature);
-        return L.marker(latlng)
-        .bindPopup("<h3>" + "Ariport code: " + feature.properties.faa + "<h3>" + "<h3>" + "Ariport name: " +feature.properties.name + "</h3>")
-        }
-    }).addTo(map);
-});
