0d0b49e91f0cc724668342e4d3c5d0747e485ff0
EMish <Liza.s.mishina@gmail.com>
2020-10-09 21:49:25 -0700

    13.4.2 final

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -5,11 +5,12 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	  maxZoom: 18,
+    id: 'mapbox/dark-v10',
+	  accessToken: API_KEY
+  });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
@@ -62,7 +63,10 @@ let cityData = cities;
 
 cityData.forEach(function(city) {
     console.log(city)
-    L.circle(city.location)
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-  .addTo(map);
+    L.circle(city.location , {
+        radius: city.population/20,
+        color: '#000000',
+        fillColor: '#F0DB4F',
+        fillOpacity: .5,
+    }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>").addTo(map);
 });
