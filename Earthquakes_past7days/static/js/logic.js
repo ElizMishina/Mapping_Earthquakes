f5b9ed4fcc50174c5183ca708a488450292ce466
EMish <Liza.s.mishina@gmail.com>
2020-10-11 11:25:35 -0700

    13.6.2

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -36,10 +36,36 @@ let baseMaps = {
     layers: [streets]
 })
 
-// Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: "#ffae42",
+          color: "#000000",
+          radius: getRadius(),
+          stroke: true,
+          weight: 0.5
+        };
+      }
+      // This function determines the radius of the earthquake marker based on its magnitude.
+      // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+      function getRadius(magnitude) {
+          if (magnitude === 0) {
+              return 1;
+            }
+            return magnitude * 4;
+        }
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+    },
+    style: styleInfo
+    
+  }).addTo(map);
 });
 
 
