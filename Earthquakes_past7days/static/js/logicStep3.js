ef6d9b30244ab1033ba740720b0e6317ded6304c
EMish <Liza.s.mishina@gmail.com>
2020-10-11 11:30:56 -0700

    13.6.3

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -31,12 +31,31 @@ let baseMaps = {
 })
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+            return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+            return "#ea822c";
+        }
+        if (magnitude > 3) {
+            return "#ee9c00";
+        }
+        if (magnitude > 2) {
+            return "#eecc00";
+        }
+        if (magnitude > 1) {
+            return "#d4ee00";
+        }
+        return "#98ee00";
+    }
 
     function styleInfo(feature) {
         return {
           opacity: 1,
           fillOpacity: 1,
-          fillColor: "#ffae42",
+          fillColor: getColor(feature.properties.mag),
           color: "#000000",
           radius: getRadius(feature.properties.mag),
           stroke: true,
@@ -57,8 +76,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         console.log(data);
         return L.circleMarker(latlng);
     },
-    style: styleInfo
-    
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }    
   }).addTo(map);
 });
 
